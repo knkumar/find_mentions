d730f9fc198fdca9f3e691a9b1140413181bb3a8
Kiran <krankumar@gmail.com>
2011-12-12 10:19:45 -0500

    finding co-ref info correctly now

--- mention_finder.py
+++ mention_finder.py
@@ -25,18 +25,10 @@ class mention_frame:
    def get_sent(self,start,end):
       return ' '.join(self.sent[start:end])
 
-def Smaller_NPs(NP,flag):
-   recur_level_NP = 0
-   def find_small_np(brackets):
-      if not (re.search("\(NP",NP[1]) if flag=='np' else re.search("\(",NP[1])):
-         return
-      else:
-         all_nps = re.findall('\(NP*',NP[1]) if flag=='np' else re.findall('\(*',NP[1])
-   nps = NP[1].index('(NP') if flag==np else NP[1].index('(')
-   stars = NP[1].find('*')
-   print NP[1].find('*',stars)
-   all_nps = re.search('\(NP',NP[1]) if flag=='np' else re.search('\(',NP[1])
+   def get_sent_full(self):
+      return ' '.join(self.sent)
 
+#find the closing brackets for the curernt tag
 def find_rindex(sent, begin, stop, i, number):
    if i >= number or stop > len(sent)-1:
       return stop
@@ -44,13 +36,15 @@ def find_rindex(sent, begin, stop, i, number):
    n_add = abs(number - sent.count('(',begin,pos))
    return find_rindex(sent, begin, pos, sent.count(')',begin,pos+1)-1, number+n_add)
 
-def count_stars(sent,start,end):
-   num_stars = sent.count('*')
-   stars_before = sent.count('*',0,start)
-   stars_after = sent.count('*',end,len(sent)) if end!=None else 0
+#count the stars between the bracket = count the number of words spanned
+def count_stars(sent,start,end,tag):
+   num_stars = sent.count('%s'%tag)
+   stars_before = sent.count('%s'%tag,0,start)
+   stars_after = sent.count('%s'%tag,end,len(sent)) if end!=None else 0
    return stars_before, num_stars-(stars_before+stars_after)
 
-def search_tag(sent,search_tag):
+#the search tag holder - returns a list of nps
+def search_tag(sent,search_tag,count_tag):
    num_np = sent.count('%s'%search_tag) 
    ret_nps = [[],[],[]]
    start = sent.find('%s'%search_tag) 
@@ -60,13 +54,14 @@ def search_tag(sent,search_tag):
       temp_end = sent.find(')',start) #first closing bracket
       end = find_rindex(sent,start+1,temp_end,0,sent.count('(',start+1,temp_end))
       end+=1
-      words,span = count_stars(sent,start,end)
+      words,span = count_stars(sent,start,end,'%s'%count_tag)
       ret_nps[0].append(words)
       ret_nps[1].append(span)
       ret_nps[2].append(sent[start:end])
       start = sent.find('%s'%search_tag,start+1) 
    return ret_nps #contains [[#words_before],[#span],[diff_nps(largest_to_smallest]] per sentence
 
+#insert the tokens into the dictionary
 def insert_tokens(sent_feat,token, num):
    if num in sent_feat.keys():
       temp  = sent_feat[num]
@@ -76,6 +71,7 @@ def insert_tokens(sent_feat,token, num):
       sent_feat[num] = [token]
    return sent_feat
 
+#extract interesting features from the data
 def extract_features(sent_features, tokens):
    sent_features = insert_tokens(sent_features, tokens[3], 3)
    sent_features = insert_tokens(sent_features, tokens[4], 4)
@@ -87,6 +83,16 @@ def extract_features(sent_features, tokens):
    sent_features = insert_tokens(sent_features, tokens[coref_col], coref_col)
    return sent_features
 
+def display_nps(sent_dict):
+   nps = sent_dict.get_nps()
+   print "+++++%s+++++"%(sent_dict.sent_number)
+   print sent_dict.get_sent_full()
+   for key in nps.keys():
+      for np in nps[key]:
+         print np[0],np[1]
+         print ''.join(np[2])
+
+#parse every sentence and add it to the object dict
 def parse_sentences(lines):
    sent_features = {}
    sent_dict = {}
@@ -100,20 +106,19 @@ def parse_sentences(lines):
              continue
           sent_features = extract_features(sent_features,tokens)
        else:
+          co_ref = max(sent_features.keys())
           for key in sent_features.keys():
              if key == 5:
-                ret_nps[5] = search_tag(''.join(sent_features[5]),"(NP")
+                ret_nps[5] = search_tag(''.join(sent_features[5]),"(NP","*")
+             elif key == co_ref:
+                ret_nps[key] = search_tag(''.join(sent_features[key]),"(","-")
              else:
-                ret_nps[key] = search_tag(''.join(sent_features[key]),"(")
+                ret_nps[key] = search_tag(''.join(sent_features[key]),"(","*")
           sent_dict[sent_num] = mention_frame(sent_num,sent_features[3],sent_features[4],sent_features[5],ret_nps)
-          nps = sent_dict[sent_num].get_nps()
-          for key in nps.keys():
-             for np in nps[key]:
-                print np[0],np[1]
-                print sent_dict[sent_num].get_sent(np[0],np[1])
-                print ''.join(np[2])
+          display_nps(sent_dict[sent_num])
           sent_num += 1
           sent_features = {}
+   return sent_dict
           
 def main():
    #"""
@@ -121,7 +126,7 @@ def main():
    lines = iter(data.readlines())
    sent_features = ['','','']
    lines.next()
-   parse_sentences(lines)
+   sent_dict = parse_sentences(lines)
    """
    test = "(NP(NP**)**)(VP*(ADJP*(PP*(NP***))))*"
    ret_np = get_nps(test,"NP")
