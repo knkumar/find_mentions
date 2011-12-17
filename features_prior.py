582e90450b53c518caff97d4b0d6f894cbf4d1cd
Kiran <krankumar@gmail.com>
2011-12-17 13:26:59 -0500

    changed some

--- features_prior.py
+++ features_prior.py
@@ -7,6 +7,7 @@ Everest has become too common , and perhaps abit too commercial
 ','(NP*(SBAR(S(S(VP*(NP*)))(VP*(VP*(ADJP(ADJP**)**(ADVP*)(ADJP(ADVP(NP**)*)*))))))))',10]"""
 import re
 import pickle
+import types
 
 class mention_frame:
    def __init__(self, number, sent, pos, bracket, nps_finder):
@@ -29,58 +30,48 @@ class mention_frame:
    def get_sent_full(self):
       return ' '.join(self.sent)
 
-def get_data(sent_dict,key):
-   nps = sent_dict[key].nps
-   coref = max(nps.keys())
-   sent = sent_dict[key].sent
-   coref_spans = map(lambda x,y: '%s|%s'%(x,x+y), nps[coref][0],nps[coref][1])
-   return nps, coref,sent, coref_spans
+def make_span(sent,pos,nps):
+   max_span = 0
+   for key in nps.keys():
+      np_list = map(lambda x,y,z: [x,x+y,z], nps[key][0],nps[key][1],nps[key][2])
+      if len(np_list) > max_span:
+         max_span = len(np_list)
+   return max_span
 
+def check_nps(sent, pos, nps):
+   return make_span(sent,pos,nps)
+                        
+def find_mentions(sent_dict):
+   all_spans = {}
+   coref_spans = {}
+   for key in sent_dict.keys(): # for every sentence
+      nps = sent_dict[key].nps
+      sent = sent_dict[key].sent
+      pos = sent_dict[key].pos_tags
+      all_spans[key] = check_nps(sent, pos, nps)
+      coref = max(nps.keys())
+      coref_span = map(lambda x,y: '%s|%s'%(x,x+y), nps[coref][0], nps[coref][1])
+      coref_spans[key] = len(coref_span)
+   return all_spans, coref_spans
 
-def insert(cluster, key):
-   if key in cluster.keys():
-      cluster[key] = cluster[key]+1
-   else:
-      cluster[key] = 1
-   return cluster
-
-def make_classes(np_list, coref, coref_spans, npkey, prior_cluster):
-   # np_list = [start,end,bracket_list]
-   for np in np_list:
-      if not np:
-         continue
-      if npkey != coref:
-         bracket = '%s'%(''.join(np[2])) # bracket string
-         prior_cluster = insert(prior_cluster,bracket)
-   return prior_cluster
-
-def find_nice_features(sent_dict):
-   prior_cluster = {}
-   #named_entity = {}
-   for key in sent_dict.keys():
-      #cannot consider features to be independent
-      nps,coref,sent,coref_spans = get_data(sent_dict,key)
-      for npkey in nps.keys():
-         if npkey == "PRN":
-            continue
-         # key:[words, span, brackets]
-         np_list = map(lambda x,y,z: [x,x+y,z], nps[npkey][0],nps[npkey][1],nps[npkey][2])
-         prior_cluster = make_classes(np_list, coref, coref_spans, npkey, prior_cluster)
-   return prior_cluster
-
-def copy_back(cluster, fname):
+def copy_back(all_pos,coref_pos, fname):
    f = open(fname, 'w')
-   for key in cluster.keys():
-      f.write('%s\t%s\n'%(key,cluster[key]))
+   total = 0
+   coref = 0
+   for key in all_pos.keys():
+      total += all_pos[key]
+   for key in coref_pos.keys():
+      coref += coref_pos[key]
+   f.write("%s"%(float(coref)/total))
    f.close()
           
 def main():
    #"""
-   sent_out = open("sent_dict.pkl","rb")
+   sent_out = open("test_dict.pkl","rb")
    sent_dict = pickle.load(sent_out)
-   print sent_dict[sent_dict.keys()[100]].nps
-   prior = find_nice_features(sent_dict)
-   copy_back(prior,'prior.txt')
+   #print sent_dict[sent_dict.keys()[100]].nps
+   all_pos, coref_pos = find_mentions(sent_dict)
+   copy_back(all_pos,coref_pos,'prior.txt')
 
 if __name__ == "__main__":
    main()
